6a90716addc8e7ce5bc86ffd69bd3f943cddd0f2
Matthew Winer <mattwiner04@gmail.com>
2024-05-12 20:12:45 -0700

    13

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
 app.get("/editor", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "editor.html"));
 });
